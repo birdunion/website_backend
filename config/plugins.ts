ed472e606c28cdaf6aa0a1c2faf285c936acacb4
Fae H-R <fae@linuxgirl.tech>
2026-09-15 13:46:49 -0600

    Added Cloudinary

--- config/plugins.ts
+++ config/plugins.ts
@@ -33,9 +33,16 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin =>
   },
   upload: {
     config: {
-      security: {
-        allowedTypes: allowedMediaTypes,
-        deniedTypes: deniedExecutableTypes,
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
   },
